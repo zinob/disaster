47f4f23832fa718638288d580e933b9bb826950b
Simon Albinsson <simon@zensum.se>
2022-01-19 14:08:18 +0100

    Making it really obvious for DAST that this is postable

--- src/se/zensum/disaster/plugins/Routing.kt
+++ src/se/zensum/disaster/plugins/Routing.kt
@@ -12,13 +12,20 @@ fun Application.configureRouting() {
     routing {
         route("/{path...}" ) {
             handle {
-                log.error("Starting")
                 var path: String = call.parameters["path"] ?: "None"
+                log.error("serving a ${call.request.httpMethod}")
                 log.error("got path: ${path}")
                 log.error("got full_path: ${call.request.path()}")
                 log.error( "got body: ${call.receiveText()}")
                 call.request.headers.forEach { k, v -> log.error("$k:$v") }
-                call.respondText("Hello World! ${path}")
+                call.respondText ("""Hello World! ${path}
+                                <form action="login" method="post">
+                                <input name="internet" type="text" /> </br>
+                                <input value="Login" type="submit" />
+                                </form>
+
+                        """.trimIndent(),
+                    ContentType.Text.Html)
             }
         }
     }
